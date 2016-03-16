72f362c37e34e4cf18f22d52c5b30805325c92fc
Hongjian <hongjian-wang@outlook.com>
2016-03-16 13:56:44 -0400

    add error message when connection failed from popup to content

--- popup.js
+++ popup.js
@@ -3,15 +3,19 @@ window.onload = function() {
 	chrome.tabs.query( {active: true}, function(tabs) {
 		tab = tabs[0];
 		$("#task").text("Crawl notes in Tab " + tab.id);
-		chrome.tabs.sendMessage( tab.id, {action: "crawl"}, null, 
+		chrome.tabs.sendMessage( tab.id, {action: "crawl"}, 
 				function(response) {
-					alert(JSON.stringify(response));
-					$(response).each( function(idx, ele) {
-						$("#notes").append('<tr><td>' + ele.id + "</td><td>" + ele.price + 
-							"</td><td>" + ele.markup + "</td><td>" + ele.ytm + "</td><td>" + 
-							ele.purpose + "</td><td>" + ele.loan_amount + "</td><td>" + 
-							ele.promise + "</td></tr>");
-					});
+					if (chrome.runtime.lastError) {
+						alert("There is a runtim error in Chrome: " + chrome.runtime.lastError.message);
+					} else {
+						alert(JSON.stringify(response));
+						$(response).each( function(idx, ele) {
+							$("#notes").append('<tr><td>' + ele.id + "</td><td>" + ele.price + 
+								"</td><td>" + ele.markup + "</td><td>" + ele.ytm + "</td><td>" + 
+								ele.purpose + "</td><td>" + ele.loan_amount + "</td><td>" + 
+								ele.promise + "</td></tr>");
+						});
+					}
 				});
 	});
 
