8858e8a2e09495eff629e6b0ef19838f1c359da0
Hongjian <hongjian-wang@outlook.com>
2016-03-18 11:54:05 -0400

    notes filter rule chain re-order

--- crawlPage.js
+++ crawlPage.js
@@ -27,22 +27,20 @@ function collectNotes(request, sender, sendRes){
 				var loan_amt = $(loan_page).find("#object-details > div:nth-child(1) tbody > tr:nth-child(3) > td").text();
 				var loan_amt_num = parseInt( loan_amt.replace(/[$,]+/g, "") );
 				note.loan_amount = loan_amt_num;
-				if (loan_amt_num <= 26000) {
-					var collection_log = $(loan_page).find("#lcLoanPerf2 tbody").html();
-					var clog_latest = $(loan_page).find("#lcLoanPerf2 tbody > tr").html();
-					var loan_status = $(loan_page).find("#object-details > div:nth-child(1) tbody > tr:nth-child(6) > td").text();
-					if (collection_log == null ) {
-						notes.push(note);
-					} else if (loan_status.indexOf("On Payment Plan") > -1) {
-						note.promise = "plan";
-						notes.push(note);
-					} else if (clog_latest.indexOf("promised to pay") > -1) {
-						note.promise = "latest promise";
-						notes.push(note);
-					} else if (collection_log.indexOf("promised to pay") > -1)  {
-						note.promise = "old promise"; 
-						notes.push(note);
-					}
+				var collection_log = $(loan_page).find("#lcLoanPerf2 tbody").html();
+				var clog_latest = $(loan_page).find("#lcLoanPerf2 tbody > tr").html();
+				var loan_status = $(loan_page).find("#object-details > div:nth-child(1) tbody > tr:nth-child(6) > td").text();
+				if (loan_status.indexOf("On Payment Plan") > -1) {
+					note.promise = "plan";
+					notes.push(note);
+				} else if (clog_latest.indexOf("promised to pay") > -1) {
+					note.promise = "latest promise";
+					notes.push(note);
+				} else if (loan_amt_num <= 26000 && collection_log.indexOf("promised to pay") > -1)  {
+					note.promise = "old promise"; 
+					notes.push(note);
+				} else if (collection_log == null ) {
+					notes.push(note);
 				}
 
 				if (cnt == rows.length) {
